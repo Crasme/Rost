9dcace6725c1ba79de16377de8ae16ce77bbd772
Loris <74245537+Lorisredstone@users.noreply.github.com>
2023-11-20 00:30:38 +0100

    print !

--- src/os.asm
+++ src/os.asm
@@ -1,5 +1,16 @@
-loop:
-  jmp loop
+mov ah, 0x0e ; mode teletype
+
+mov al, 'R'
+int 0x10
+mov al, 'O'
+int 0x10
+mov al, 'S'
+int 0x10
+mov al, 'T'
+int 0x10
+
+boucle:
+    jmp boucle
 
 times 510 -( $ - $$ ) db 0
 
